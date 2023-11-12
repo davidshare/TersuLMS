e22ab51fc12445efc4fe1ff4cad7f40d72eef9db
David Essien <davidessienshare@gmail.com>
2023-11-12 17:22:55 +0100

    fix config error

--- src/config.py
+++ src/config.py
@@ -2,14 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     APP_NAME: str = "LMS API"
-    POSTGRES_USER="postgres"
-    POSTGRES_PASSWORD="postgres"
-    POSTGRES_DATABASE="lmsdb"
-    POSTGRES_PORT="5432"
-    POSTGRES_HOST="127.0.0.1"
-    POSTGRES_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DATABASE: str = "lmsdb"
+    POSTGRES_PORT: str = "5432"
+    POSTGRES_HOST: str = "127.0.0.1"
+    POSTGRES_URI: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
 
     model_config = SettingsConfigDict(env_file=".env")
-
 
 settings = Settings()
