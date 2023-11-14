d6db095c75d760cf68ccf3d4196a30f5fcd774c5
David Essien <davidessienshare@gmail.com>
2023-11-14 10:24:20 +0100

    add email verification model

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ A learning management system
 ## Start the app
 `uvicorn src.main:app --reload`
 
+## Run migrations
+`alembic revision --autogenerate -m "<description of migration>"` [//]: <> Create the migration
+`alembic upgrade head` [//]: <> update the database
+
 ## Important urls
 ### FastAPI documentation
 - http://<HOST>:8000/doc
--- src/auth/models.py
+++ src/auth/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy import Column, Integer, String, ForeignKey, Date, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
@@ -14,17 +14,28 @@ class HashingAlgorithm(Base):
 
 class UserLoginData(Base):
     __tablename__ = 'user_login'
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     login_name = Column(String(20))
     password_hash = Column(String(250))
     password_salt = Column(String(100))
     hash_algorithm_id = Column(Integer, ForeignKey('hashing_algorithms.id'))
     email = Column(String(100))
-    confirmation_token = Column(String(100))
-    token_generation_time = Column(Date)
     is_active = Column(Boolean, default=False)
     password_recovery_token = Column(String(100))
     recovery_token_time = Column(Date)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     hashing_algorithms = relationship("HashingAlgorithms")
+
+class EmailVerification(Base):
+    __tablename__ = 'email_verification'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    user_id = Column(Integer, ForeignKey('user_login.id'))
+    confirmation_token = Column(String(100))
+    token_generation_time = Column(Date)
+    is_active = Column(Boolean, default=False)
+    is_used = Column(Boolean, default=False)
+    expired_at = Column(DateTime, nullable=False, default=lambda: datetime.utcnow() + timedelta(minutes=15))
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    user_login = relationship("UserLoginData")
