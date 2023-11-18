38b560dea77ce17c0056a5d3e46216be0ab585ab
David Essien <davidessienshare@gmail.com>
2023-11-18 08:54:34 +0100

    reorganise roles

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -4,6 +4,8 @@ from .controller import AuthController
 from .schemas import UserAuthCreate, HashingAlgorithmCreate, UserAuthResponse, UserAuthLogin, TokenResponse,UserRoleCreate, UserRoleResponse
 
 router = APIRouter()
+hashing_router = APIRouter()
+user_roles_router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
@@ -25,57 +27,57 @@ def refresh_token(token: str = Depends(oauth2_scheme)):
     return AuthController.refresh_token(token)
 
 
-@router.post("/hashing-algorithms/")
+@hashing_router.post("/hashing-algorithms/")
 def create_hashing_algorithm(algorithm_name: HashingAlgorithmCreate):
     """API endpoint to create a new hashing algorithm."""
     return AuthController.create_hashing_algorithm(algorithm_name)
 
 
-@router.get("/hashing-algorithms/")
+@hashing_router.get("/hashing-algorithms/")
 def get_all_hashing_algorithms():
     """Get all hashing algorithms."""
     return AuthController.get_all_hashing_algorithms()
 
 
-@router.get("/hashing-algorithms/{algorithm_id}")
+@hashing_router.get("/hashing-algorithms/{algorithm_id}")
 def get_hashing_algorithm_by_id(algorithm_id: int):
     """Get a hashing algorithm by its ID."""
     return AuthController.get_hashing_algorithm_by_id(algorithm_id)
 
 
-@router.get("/hashing-algorithms/name/{algorithm_name}")
+@hashing_router.get("/hashing-algorithms/name/{algorithm_name}")
 def get_hashing_algorithm_by_name(algorithm_name: str):
     """Get a hashing algorithm by its name."""
     return AuthController.get_hashing_algorithm_by_name(algorithm_name)
 
 
-@router.put("/hashing-algorithms/{algorithm_id}")
+@hashing_router.put("/hashing-algorithms/{algorithm_id}")
 def update_hashing_algorithm(algorithm_id: int, algorithm_name: HashingAlgorithmCreate):
     """API endpoint to update a hashing algorithm."""
     return AuthController.update_hashing_algorithm(algorithm_id, algorithm_name.algorithm_name)
 
 
-@router.delete("/hashing-algorithms/{algorithm_id}")
+@hashing_router.delete("/hashing-algorithms/{algorithm_id}")
 def delete_hashing_algorithm(algorithm_id: int):
     """API endpoint to delete a hashing algorithm."""
     return AuthController.delete_hashing_algorithm(algorithm_id)
 
-@router.post("/users/roles/", status_code=status.HTTP_200_OK, response_model=UserRoleResponse)
+@user_roles_router.post("/users/roles/", status_code=status.HTTP_200_OK, response_model=UserRoleResponse)
 def create_user_role(role_name: UserRoleCreate):
     """API endpoint to create a new user role."""
     return AuthController.create_role(role_name)
 
-@router.get("/users/roles/", status_code=status.HTTP_200_OK, response_model=list[UserRoleResponse])
+@user_roles_router.get("/users/roles/", status_code=status.HTTP_200_OK, response_model=list[UserRoleResponse])
 def get_all_user_roles():
     """API endpoint to get all user roles."""
     return AuthController.get_all_roles()
 
-@router.get("/users/roles/{role_id}", status_code=status.HTTP_200_OK, response_model=UserRoleResponse)
+@user_roles_router.get("/users/roles/{role_id}", status_code=status.HTTP_200_OK, response_model=UserRoleResponse)
 def get_user_role_by_id(role_id: int):
     """API endpoint to get a user role by ID."""
     return AuthController.get_role_by_id(role_id)
 
-@router.get("/users/roles/{role_name}", status_code=status.HTTP_200_OK, response_model=UserRoleResponse)
+@user_roles_router.get("/users/roles/{role_name}", status_code=status.HTTP_200_OK, response_model=UserRoleResponse)
 def get_user_role_by_name(role_name: str):
     """API endpoint to get a user role by name."""
     return AuthController.get_role_by_name(role_name)
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
-from .auth.routes import router as auth_router
+from .auth.routes import router as auth_router, hashing_router, user_roles_router
 
 global_router = APIRouter()
 
 global_router.include_router(auth_router, prefix="/api/v1/auth", tags=["auth"])
+global_router.include_router(
+    hashing_router, prefix="/api/v1/auth", tags=["hashing-algorithms"])
+global_router.include_router(
+    user_roles_router, prefix="/api/v1/auth", tags=["user-roles"])
