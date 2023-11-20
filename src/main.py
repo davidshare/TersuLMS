6d722be5eebb2dbd3d297584ea8525392f9267bb
David Essien <davidessienshare@gmail.com>
2023-11-20 22:37:12 +0100

    chore(models): update models

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .models import *
 from .routes import global_router
 from .config.settings import settings
 
@@ -8,3 +9,7 @@ app.include_router(global_router)
 @app.get("/")
 def read_root():
     return {"Hello": settings.APP_NAME}
+
+#TODO: use chatgpt to check for more ideas to improve the models and how to better design them
+#TODO: update relationships between models to be more efficient
+#TODO: update models to return complete data for specific endpoints
